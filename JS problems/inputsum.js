a397f4f7afe4a4e4348be7f2ad356d0c1a54d150
shbej <sh.bejanyan@gmail.com>
2020-07-16 17:31:09 +0400

    Update inputsum.js

--- JS problems/inputsum.js
+++ JS problems/inputsum.js
@@ -1,7 +1,6 @@
 //Create a program that will print the sum of two inputted numbers
 
-let a = prompt('Hi, please enter a number here');
-let b = prompt('And another one here');
+const a = prompt('Hi, please enter a number here');
+const b = prompt('And another one here');
 
-alert(`The sum of your 2 numbers is ${Number(a) + Number(b)}!`)
-
+alert(`The sum of your 2 numbers is ${Number(a) + Number(b)}!`);
